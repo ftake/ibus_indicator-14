c6299313ce247bb9f8ab47c96ef59eb9b1f6ffd8
Fuminobu <ftake@geeko.jp>
2013-02-13 03:06:20 +0900

    - Fix preventing other indicators to show - Implement removing icon

--- extension.js
+++ extension.js
@@ -1,23 +1,33 @@
-const Main = imports.ui.main;
-const Shell = imports.gi.Shell;
-const Panel = imports.ui.panel;
-const Lang = imports.lang;
-const PanelMenu = imports.ui.panelMenu;
+const Main = imports.ui.main
+const Shell = imports.gi.Shell
+const Panel = imports.ui.panel
+const Lang = imports.lang
+const PanelMenu = imports.ui.panelMenu
+const IBus = imports.gi.IBus
 
 const AddIndicator = new Lang.Class({
     Name: 'AddIndicator',
     _init: function() {
-	this._trayManager = new Shell.TrayManager()
-	this._trayManager.connect('tray-icon-added', Lang.bind(this, this._onTrayIconAdded))
-	this._trayManager.manage_stage(global.stage, Main.messageTray.actor);
+	Main.notificationDaemon._trayManager.connect('tray-icon-added', Lang.bind(this, this._onTrayIconAdded))
+	Main.notificationDaemon._trayManager.connect('tray-icon-removed', Lang.bind(this, this._onTrayIconRemoved))
     },
 
     _onTrayIconAdded: function(manager, actor) {
-	if (actor.wm_class == 'ibus-ui-gtk-14') {
-	    let button = new PanelMenu.Button();
-            button.actor.add_actor(actor);
-	    Main.panel.addToStatusArea('ibus-gtk-ui-14', button, 0, 'right')
+	if (actor.wm_class == 'ibus-ui-gtk') {
+	    if (!('new_async' in IBus.Bus)) {
+		// This IBus does not support GNOME 3.6
+		actor.height = Panel.PANEL_ICON_SIZE
+		this.button = new PanelMenu.Button()
+		this.button.actor.add_actor(actor)
+		Main.panel.addToStatusArea('ibus-gtk-ui', this.button, 0, 'right')
+	    }
 	}
+    },
+
+    _onTrayIconRemoved: function(manager, actor) {
+	if (actor.wm_class == 'ibus-ui-gtk') {
+	    this.button.destroy()
+	}	
     }
 })
 
